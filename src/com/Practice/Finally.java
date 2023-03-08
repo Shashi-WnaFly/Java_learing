70b9262c8929d451822411d7ba678135f17e6fd1
Shashi <shashianand2600@gmail.com>
2023-03-08 16:13:08 +0530

    Practice of custom exception making.

--- src/com/Practice/Finally.java
+++ src/com/Practice/Finally.java
@@ -1,37 +1,43 @@
 package com.Practice;
+
 import java.util.Scanner;
 
-class ArithException extends Exception{
-    public String toString(){
+class ArithException extends Exception {
+    public String toString() {
         return super.toString() + " : Please use the number greater than 0 ";
     }
-    public String getMessage(){
-        return super.toString() + " Please use the number greater than 0 ";
+
+    public String getMessage() {
+        return super.getMessage() + " Please use the number greater than 0 ";
     }
 }
+
 public class Finally {
-    public static int groo(int n)throws ArithException{
-        if(n<=0){
+    public static float groo(float n) throws ArithException {
+        float x = 102;
+        if (n < 1) {
             throw new ArithException();
         }
-        else{
-            return n;
-        }
+        return (x / n);
     }
-    public static void main(String[] args){
-        int x = 102, y;
+
+    public static void main(String[] args) {
         System.out.println("Please enter a nunber to divide a unknown number.");
         Scanner yo = new Scanner(System.in);
 //        boolean check = true;
-        while(true){
-            int num = yo.nextInt();
-            try{
-                System.out.println("Here is the result of calculation : "+x/groo(num));
+        while (true) {
+            try {
+                float num = yo.nextFloat();
+                System.out.println("Here is the result of calculation : " + groo(num));
                 break;
             }
-            finally{  //Even though program had been break but finally keyword express itself.
+             catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+             finally {  //Even though program had been break but finally keyword express itself.
                 System.out.println("Yes It is finally.");
             }
         }
     }
 }
+
