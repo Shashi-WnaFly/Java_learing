d0bbfb09f4308a56a8589bf852dd9340e2de1274
Shashi <shashianand2600@gmail.com>
2023-03-22 23:08:09 +0530

    Initial commit

---
+++ src/com/Practice/ParsInt.java
@@ -0,0 +1,11 @@
+package com.Practice;
+import java.util.*;
+public class ParsInt {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter any string.");
+        String go = in.next();
+        int h = Integer.parseInt(go);
+        System.out.println((10+h));
+    }
+}
