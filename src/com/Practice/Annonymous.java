4f0530edeb9c3178de7c149933652b016ac15e64
Shashi <shashianand2600@gmail.com>
2023-03-31 14:01:58 +0530

    Annonymous object practice and lambda expression

--- src/com/Practice/Annonymous.java
+++ src/com/Practice/Annonymous.java
@@ -1,22 +1,39 @@
 package com.Practice;
 interface lotus{
-    void redLotus();
+//    void redLotus();
     void pinkLotus();
+}
+class daffodil implements lotus{
+    public void pinkLotus(){
+        System.out.println("Pink Lotus are one of my favorite");
+    }
+    public void redLotus(){
+        System.out.println("Red Lotus are one of my favorite");
+    }
 }
 public class Annonymous {
     public static void main(String[] args){
-        lotus up = new lotus() {
-            @Override
-            public void redLotus(){
-                System.out.println("Hi... I am a Red Lotus.");
-            }
+        // Annonymous class
 
-            public void pinkLotus(){
-                System.out.println("Hi... I am a Pink Lotus");
-            }
+//        lotus up = new lotus() {
+//            @Override
+//            public void redLotus(){
+//                System.out.println("Hi... I am a Red Lotus.");
+//            }
+
+//            public void pinkLotus(){
+//                System.out.println("Hi... I am a Pink Lotus");
+//            }
+//        };
+//        up.pinkLotus();
+//        up.redLotus();
+        lotus down = new daffodil();
+        down.pinkLotus();
+
+        //lamda Expression
+        lotus kilo = ()->{
+            System.out.println("This is from lambda expressions.");
         };
-        up.pinkLotus();
-        up.redLotus();
-
+        kilo.pinkLotus();
     }
 }
