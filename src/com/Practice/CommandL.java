72539dbabc7aa125cd82e3d17ea181f211bb312f
Shashi <shashianand2600@gmail.com>
2023-04-13 19:38:25 +0530

    initial commit

---
+++ src/com/Practice/CommandL.java
@@ -0,0 +1,11 @@
+package com.Practice;
+
+public class CommandL {
+    public static void main(String []args) {
+        int i, l;
+        l = args.length;
+        for(i = 0; i<l; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
